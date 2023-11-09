0efc2eedc0d28c38b7deb0ced6743ec2bb94cebe
Simon Biffe <sone359@outlook.fr>
2023-11-09 17:15:11 +0100

    Résolutions de bugs concernant pyramide.cpp, nettoyage du répertoire et ajout de tests dans le main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
+#include "stockGemmes.h"
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello world" << endl;
+    //Tests concernant StockGemmes
+    StockGemmes stockTest;
+    cout << stockTest.get_Blanc() << endl;
+    StockGemmes stockTest2(1, 2, 3, 4, 2, 1);
+    cout << stockTest2.get_Blanc() << endl;
     return 0;
 }
