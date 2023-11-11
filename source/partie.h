c40796030a095ad23878e5f02681ab7be564b137
Marie Herminie <marie-herminie.blondy@etu.utc.fr>
2023-11-11 20:39:56 +0100

    à l'aide ça marche pas

--- source/partie.h
+++ source/partie.h
@@ -2,14 +2,18 @@
 #define PARTIE_H
 
 #include "pyramide.h"
-#include "carte_joaillerie.h"
+#include "carte_joaillerie_test.h"
 
 
 class Partie{
     private :
     
+        CarteJoaillerie * cartes[67];
     public : 
-
+        
+        void init(){
+            Pyramide::initialiser(cartes);
+        }
 };
 
 #endif
