8cf69b2bf1b0be81ba07c2e23c37c05fcae463dc
Marie Herminie <marie-herminie.blondy@etu.utc.fr>
2023-11-14 17:58:10 +0100

    lien partie-pyramide

--- source/partie.h
+++ source/partie.h
@@ -11,9 +11,21 @@ class Partie{
     public : 
         CarteJoaillerie * cartes[67];
         
-        void init(){
+        void initPyramide(){
             Pyramide::initialiser(cartes);
             Pyramide::getInstance()->afficherPyramide();
+        }
+
+        void initPlateau(){
+            
+        }
+
+        Partie(){
+        for (int i=0;i<67;i++){cartes[i]=new CarteJoaillerie(i,i);};
+            initPyramide();
+        }
+        ~Partie(){
+        for (int i=0;i<67;i++){delete cartes[i];}
 
         }
 };
