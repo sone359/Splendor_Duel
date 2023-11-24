a250b4fe84300fb0421f5b04b59b1c485016f702
Simon Biffe <sone359@outlook.fr>
2023-11-24 21:45:45 +0100

    Correction du caractère déterministe de la séquence pseudo-aléatoire de tirage des jetons du Sac en remplaçant la seed

--- source/Back/sac.h
+++ source/Back/sac.h
@@ -24,7 +24,6 @@ protected:
 
 private:
     StockGemmesOr gemmes;
-    std::random_device random_seed; //Attribut car en raison du caractère pseudo-aléatoire, il y a nécessité d'unicité pour ne pas obtenir la même séquence de nombre à chaque appel
 };
 
 #endif
